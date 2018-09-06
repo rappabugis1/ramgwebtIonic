44ac9b25e5216319d2d7b50f4871003442788845
rappabugis1 <rappabugis1@etf.unsa.ba>
2018-09-06 20:09:00 +0200

    Added navigation buttons for productSlides

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component , ViewChild} from '@angular/core';
-import { NavController, Platform, Content } from 'ionic-angular';
+import { NavController, Platform, Content, Slides } from 'ionic-angular';
 
 import {ScrollHideConfig} from '../../directives/headerhider';
 
@@ -13,7 +13,9 @@ import { ProductPage } from '../product/product';
 export class HomePage {
 
   @ViewChild(Content) content: Content;
+  @ViewChild('productSlides') slides: Slides;
 
+  platW;
   meniSakriven:boolean =false;
   public products;
   slidesPerView :number=1;
@@ -24,15 +26,16 @@ export class HomePage {
 
   constructor(public navCtrl: NavController,private fireStoreProvider: FirestoreProvider, public platform:Platform) {
     this.products = this.fireStoreProvider.getProductList().valueChanges();
+    this.platW=this.platform.width();
 
-    if(this.platform.width() > 750)
+    if(this.platW > 750)
     this.slidesPerView=3;
     else
-      if(this.platform.width() > 450)
+      if(this.platW > 450)
       this.slidesPerView=2;
 
 
-    if(this.platform.width() > 450) {
+    if(this.platW > 450) {
       this.slide1="assets/imgs/slide1.jpg";
       this.slide2="assets/imgs/hope.jpg";
       this.slide3="assets/imgs/lopata.jpg";
@@ -69,6 +72,14 @@ export class HomePage {
 
   proslijediProduct(prod){
     this.navCtrl.push(ProductPage, {prod: prod, products:this.products });
+  }
+
+  buttonDesno(){
+    this.slides.slideNext();
+  }
+
+  buttonLijevo(){
+    this.slides.slidePrev();
   }
 
 }
